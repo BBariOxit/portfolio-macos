a0fc835eba248acbc18a209528ec42edfe37f452
phanthaibao <phanbao2648@gmail.com>
2026-04-05 11:45:26 +0700

    implement a hoc for managing window states and add the first window-terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import gsap from "gsap"
-import { Draggable } from "gsap/Draggable"
+import { Draggable } from "gsap/draggable"
 
 import { Dock, Navbar, Welcome } from "@components"
 import { Terminal } from "@windows"
